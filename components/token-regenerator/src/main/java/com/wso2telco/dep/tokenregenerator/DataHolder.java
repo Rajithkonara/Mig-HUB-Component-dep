1cf32d9c78b0a8e8b2399af8b308b036c906f525
Rajithkonara <rajithk@wso2.com>
2018-11-01 11:46:53 +0530

    adding logs and refactoring

--- components/token-regenerator/src/main/java/com/wso2telco/dep/tokenregenerator/DataHolder.java
+++ components/token-regenerator/src/main/java/com/wso2telco/dep/tokenregenerator/DataHolder.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018, WSO2.Telco Inc. (http://www.wso2telco.com)
+ *
+ * All Rights Reserved. WSO2.Telco Inc. licences this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.wso2telco.dep.tokenregenerator;
 
 import org.wso2.carbon.apimgt.impl.APIManagerConfigurationService;
@@ -5,10 +20,9 @@ import org.wso2.carbon.user.core.service.RealmService;
 
 public class DataHolder {
 
-    private static RealmService realmService;
+    private  RealmService realmService;
     private static volatile DataHolder dataHolder;
-    private static APIManagerConfigurationService apiManagerConfigurationService;
-    private static TokenRegenerationEventListener tokenRegenerationEventListener;
+    private APIManagerConfigurationService apiManagerConfigurationService;
 
     private DataHolder() {
 
@@ -21,7 +35,6 @@ public class DataHolder {
             synchronized (DataHolder.class) {
                 if (dataHolder == null) {
                     dataHolder = new DataHolder();
-                    tokenRegenerationEventListener = new TokenRegenerationEventListener();
                 }
             }
 
@@ -38,15 +51,10 @@ public class DataHolder {
         return realmService;
     }
 
-    public TokenRegenerationEventListener getCustomUserOperationEventListener() {
-        return tokenRegenerationEventListener;
-    }
-
     public void setApiManagerConfigurationService(APIManagerConfigurationService apiManagerConfigurationService) {
         this.apiManagerConfigurationService = apiManagerConfigurationService;
     }
 
     public APIManagerConfigurationService getApiManagerConfigurationService() {return apiManagerConfigurationService;}
-
 
 }
--- components/token-regenerator/src/main/java/com/wso2telco/dep/tokenregenerator/TokenRegenerationEventListener.java
+++ components/token-regenerator/src/main/java/com/wso2telco/dep/tokenregenerator/TokenRegenerationEventListener.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018, WSO2.Telco Inc. (http://www.wso2telco.com)
+ *
+ * All Rights Reserved. WSO2.Telco Inc. licences this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.wso2telco.dep.tokenregenerator;
 
 import org.apache.log4j.Logger;
@@ -8,17 +23,19 @@ import org.wso2.carbon.identity.oauth.OAuthAdminService;
 import org.wso2.carbon.identity.oauth.dto.OAuthConsumerAppDTO;
 import org.wso2.carbon.identity.oauth2.OAuth2Service;
 import org.wso2.carbon.identity.oauth2.dto.OAuth2AccessTokenReqDTO;
+import org.wso2.carbon.identity.oauth2.dto.OAuth2AccessTokenRespDTO;
 import org.wso2.carbon.identity.oauth2.model.RequestParameter;
 import org.wso2.carbon.user.core.UserStoreException;
 import org.wso2.carbon.user.core.UserStoreManager;
 import org.wso2.carbon.user.core.common.AbstractUserOperationEventListener;
 
-import static org.wso2.carbon.identity.application.authentication.framework.util.FrameworkConstants.AUDIT_MESSAGE;
-import static org.wso2.carbon.registry.core.session.CurrentSession.getUser;
-
 public class TokenRegenerationEventListener extends AbstractUserOperationEventListener {
 
     private Logger log = Logger.getLogger(TokenRegenerationEventListener.class);
+    private static final String VALIDITY_PERIOD_KEY = "validity_period";
+    private static final String VALIDITY_PERIOD_VALUE = "-2";
+    private static final String TENANT_DOMAIN = "carbon.super";
+    private static final String GRANT_TYPE = "client_credentials";
 
     @Override
     public int getExecutionOrderId() {
@@ -27,46 +44,81 @@ public class TokenRegenerationEventListener extends AbstractUserOperationEventLi
         return 1356;
     }
 
+    /**
+     * Refresh token on password reset
+     * @param userName username
+     * @param credential password
+     * @param userStoreManager userStoreManager
+     * @return true
+     * @throws UserStoreException
+     */
     @Override
     public boolean doPostUpdateCredential(String userName, Object credential, UserStoreManager userStoreManager) throws UserStoreException {
 
+        if (log.isDebugEnabled()) {
+            log.debug("Executing TokenRegenerationEventListener");
+            log.debug("Username : "+ userName);
+        }
 
         OAuthAdminService oAuthAdminService = new OAuthAdminService();
         String[] applicationScope;
-        String instancr=  DataHolder.getInstance().getApiManagerConfigurationService().getAPIManagerConfiguration().getFirstProperty(APIConstants.
+        String scope =  DataHolder.getInstance().getApiManagerConfigurationService().getAPIManagerConfiguration().getFirstProperty(APIConstants.
                 APPLICATION_TOKEN_SCOPE);
 
-        applicationScope = new String[] {instancr};
+        applicationScope = new String[] {scope};
 
         try {
             OAuth2AccessTokenReqDTO tokenReqDTO = new OAuth2AccessTokenReqDTO();
             RequestParameter[] requestParameters = new RequestParameter[1];
-            RequestParameter requestParameter = new RequestParameter("validity_period","-2");
+            RequestParameter requestParameter = new RequestParameter(VALIDITY_PERIOD_KEY,VALIDITY_PERIOD_VALUE);
 
             PrivilegedCarbonContext.startTenantFlow();
             PrivilegedCarbonContext.getThreadLocalCarbonContext().setUsername(userName);
-            PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain("carbon.super");
+            PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain(TENANT_DOMAIN);
             PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantId(-1234);
-            OAuthConsumerAppDTO[] outhapps0 = oAuthAdminService.getAllOAuthApplicationData();
 
-            tokenReqDTO.setGrantType("client_credentials");
-            tokenReqDTO.setClientId(outhapps0[0].getOauthConsumerKey());
-            tokenReqDTO.setClientSecret(outhapps0[0].getOauthConsumerSecret());
-            tokenReqDTO.setCallbackURI(outhapps0[0].getCallbackUrl());
-            tokenReqDTO.setScope(applicationScope);
-            tokenReqDTO.setTenantDomain(null);
-            tokenReqDTO.setPkceCodeVerifier(null);
+            if (log.isDebugEnabled()) {
+                log.debug("Fetching application details ");
+            }
+            OAuthConsumerAppDTO[] oauthapps = oAuthAdminService.getAllOAuthApplicationData();
 
-            requestParameters[0] = requestParameter;
-            tokenReqDTO.setRequestParameters(requestParameters);
-            tokenReqDTO.setRequestParameters(requestParameters);
-            OAuth2Service service = (OAuth2Service) PrivilegedCarbonContext.getThreadLocalCarbonContext().getOSGiService(OAuth2Service.class);
-            service.issueAccessToken(tokenReqDTO);
-            PrivilegedCarbonContext.endTenantFlow();
-            return true;
+            if (oauthapps != null) {
+                tokenReqDTO.setGrantType(GRANT_TYPE);
+                tokenReqDTO.setClientId(oauthapps[0].getOauthConsumerKey());
+                tokenReqDTO.setClientSecret(oauthapps[0].getOauthConsumerSecret());
+                tokenReqDTO.setCallbackURI(oauthapps[0].getCallbackUrl());
+                tokenReqDTO.setScope(applicationScope);
+                tokenReqDTO.setTenantDomain(null);
+                tokenReqDTO.setPkceCodeVerifier(null);
+
+                if (log.isDebugEnabled()) {
+                    log.debug("Application Details Fetched Successfully \n" +
+                              "Application name:  "+oauthapps[0].getApplicationName() + "\n" +
+                              "Consumer_key: "+oauthapps[0].getOauthConsumerKey() + "\n" +
+                              "Consumer_secret: "+oauthapps[0].getOauthConsumerSecret()
+                    );
+                }
+
+                requestParameters[0] = requestParameter;
+                tokenReqDTO.setRequestParameters(requestParameters);
+                tokenReqDTO.setRequestParameters(requestParameters);
+                OAuth2Service service = (OAuth2Service) PrivilegedCarbonContext.getThreadLocalCarbonContext()
+                        .getOSGiService(OAuth2Service.class);
+                if(log.isDebugEnabled()) {
+                    log.debug("Starting to issue AccessToken");
+                }
+
+                OAuth2AccessTokenRespDTO oAuth2AccessTokenRespDTO = service.issueAccessToken(tokenReqDTO);
+
+                if (log.isDebugEnabled()) {
+                    log.debug("Generated Access Token :" +oAuth2AccessTokenRespDTO.getAccessToken());
+                }
+
+                PrivilegedCarbonContext.endTenantFlow();
+            }
 
         } catch (IdentityOAuthAdminException e) {
-            log.error("Service Error");
+            log.error("Error while Fetching application details ", e);
         }
 
         return true;
